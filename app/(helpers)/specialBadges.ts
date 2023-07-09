4e6ae7575e27dc4a8651d83c188512d624b816e6
wesngu28 <wesley@wesngu28.com>
2023-07-09 09:59:10 -0700

    feat: add more badges

--- app/(helpers)/specialBadges.ts
+++ app/(helpers)/specialBadges.ts
@@ -1,14 +1,21 @@
 export const banners: {[key: string]: string} = {
     "Game Mod": "mod_status",
     "WA": "wa_status",
+    "Delegate": "wa_status",
+    "Secretariat": "wa_status",
     "Issues Editor": "semimod_status",
     "Roleplay Mentor": "mentor_status",
     "Class": "class_status"
 }
 export const badges: {[key: string]: string} = {
     "Founder": "founder",
-    "Retired Moderator": "mod",
+    "Retired Moderator": "modex",
     "Easter Egg": "eegg1",
     "Issues Author": "issuesauthor",
-    "Postmaster": "acc_supporter_silver"
+    "Postmaster": "acc_supporter_silver",
+    "Postmaster-General": "acc_supporter",
+    "Historical Resolution Author": "resauthorun",
+    "General Assembly Resolution Author": "resauthorga",
+    "Moderator": "mod",
+    "Site Supporter": "acc_supporter_bronze"
 }
