82a44d5544b25db0a327aed92917a9001c9f88c7
wesngu28 <wesngu028@gmail.com>
2023-07-19 14:56:37 -0700

    fix: prod versions

--- backend/cache.py
+++ backend/cache.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 def create_redis():
   return redis.ConnectionPool(
-    host='localhost', 
-    port=6379,
+    host=os.environ['REDISHOST'], 
+    port=os.environ['REDISPORT'],
     password=os.environ['REDISPASSWORD'] if 'REDISPASSWORD' in os.environ else '',
     db=0, 
     decode_responses=True
--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cards.db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if 'sqlite' in database_url:
+    engine = create_engine(database_url, connect_args={'check_same_thread': False} )
+else:
+    engine = create_engine(database_url, connect_args={})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
