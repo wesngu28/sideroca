318a5e3d50415cee24ea364c542d42b2b2295868
wesngu28 <wesngu028@gmail.com>
2023-07-19 15:04:35 -0700

    fix: idek

--- backend/database.py
+++ backend/database.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
-if 'sqlite' in database_url:
-    engine = create_engine(database_url, connect_args={'check_same_thread': False} )
+if 'sqlite' in SQLALCHEMY_DATABASE_URL:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False} )
 else:
-    engine = create_engine(database_url, connect_args={})
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
