ab698b86fb6a2dc6171a77d7b4516513e3a50de2
wesngu28 <wesngu028@gmail.com>
2023-07-20 20:37:02 -0700

    chore: minor documentation

--- frontend/app/docs/page.tsx
+++ frontend/app/docs/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react";
+import { Home, Paperclip } from "lucide-react";
 import Link from "next/link";
-import { badges, governments, flags, trophiesDict } from "../(helpers)/categories";
 
 export default function Docs() {
     return (
@@ -12,13 +11,39 @@ export default function Docs() {
             </div>
             <Link className="mt-3 mb-6 transition-colors duration-300 hover:text-purple-700" href="https://nscards.up.railway.app/cards">https://nscards.up.railway.app/cards</Link>
             <div className="flex flex-col gap-8 max-w-[300px] phone:max-w-5xl">
+                <div>
                 <p className="leading-7 [&:not(:first-child)]:mt-6">
-                    Card Queries is the frontend over an API made with FastAPI, a python library for making fast apis. The API
-                    grants access to the dumps which are stored in a PostgreSQL database. At the moment, manually building queries
-                    offers more access than the interface.
+                    This serves as the frontend for an API built with FastAPI, a powerful Python library for creating fast APIs. Our API provides access to data stored in PostgreSQL dumps. 
+                    While the frontend offers a user-friendly interface, I acknowledge that manual query building currently provides more extensive access to the data.
+                </p>
+                <p className="leading-7 mt-4">
+                    The API primarily serves as a card query facilitator. It enables users to retrieve card-related information through 
+                    various queries. However, specific features related to collections and decks are handled exclusively on the frontend. 
+                    These functionalities rely on making API calls to NationStates, and to mitigate potential rate-limiting issues, I've 
+                    chosen to keep these operations on the client-side.
+                </p>
+                </div>
+                <h2 className="scroll-m-20 border-b text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    Interface
+                </h2>
+                <p className="leading-7">
+                    The exclamation button (!) is used to negate queries. When you add the exclamation symbol before a field or parameter, 
+                    the interface will perform a negated query, which means it will return results that do not match the specified condition.
+                    The plus button (+) allows you to add additional fields to your queries. When you click on the plus button, it opens up 
+                    a menu of available fields that you can include in your query.
+                </p>
+                <p className="leading-7">
+                    At the top of the interface, you will find a button that allows you to switch between the "/cards" and "/collection" routes. 
+                    When you switch between the routes, the interface will adapt to display the relevant options and functionalities specific to that route.
+                    On the "/cards" route, you have the option to select between "Collection" and "Deck", which is a mutually exclusive choice.
+                </p>
+                <p className="leading-7">
+                    For users who are comfortable with manual query building, you have the option to construct your own custom query. At the top of the interface, 
+                    there is an input area where you can specify the parameters for your query. When building, leave out the route. For example, do 
+                    nation=testlandia instead of /cards?nation=testlandia.
                 </p>
                 <h2 className="scroll-m-20 border-b text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                    Routes
+                    API
                 </h2>
                 <div className="flex gap-4 pb-2">
                 <a href="#cards" className="transition-colors duration-300 hover:text-purple-700 leading-7">/cards</a>
@@ -68,15 +93,21 @@ export default function Docs() {
                     </h4>
                     <p className="leading-7 [&:not(:first-child)]:mt-2">
                         You can exclude certain parameters from the query by simply not including them. However, this does not mean you are searching for nations without those fields.
+                    </p>
+                    <p className="leading-7 [&:not(:first-child)]:mt-2">
                         To explicitly exclude them, you can use an exclamation mark before the parameter (e.g., category=!anarchy).
-                        Trophies and badges are special as they support full exclusion searching. You can use the keyword sans to find nations without trophies (e.g., trophies=sans).
+                        Trophies and badges are special as they support full exclusion searching. You can use the keyword sans to find nations 
+                        without trophies (e.g., trophies=sans).
                     </p>
                     <h4 className="scroll-m-20 text-xl font-semibold tracking-tight mt-6">
                         Partial and Exact Matching
                     </h4>
                     <p className="leading-7 [&:not(:first-child)]:mt-2">
                         For some parameters, you can perform both partial and exact matching. These parameters include name, nation manual, type, region, and motto.
-                        By default, they perform partial matching. To match exactly, you can use double equal signs (e.g., name==Giovanni matches Giovanni exactly).
+                        By default, they perform partial matching.
+                    </p>
+                    <p className="leading-7 [&:not(:first-child)]:mt-2">
+                        To match exactly, you can use double equal signs (e.g., name==Giovanni matches Giovanni exactly).
                         You can also use the != operator to ignore a specific value (e.g., name=!=Giovanni ignores the nation named Giovanni).
                     </p>
                     <h4 className="scroll-m-20 text-xl font-semibold tracking-tight mt-6">
@@ -100,17 +131,21 @@ export default function Docs() {
                     <h4 className="scroll-m-20 text-xl font-semibold tracking-tight mt-6">
                         Exposed Parameters
                     </h4>
+                    <Link className="font-semibold my-4 flex gap-2" href={"/docs/parameters"}>
+                        <Paperclip />
+                        List of Valid Parameters
+                    </Link>
                     <p className="leading-7">
                         All parameters can be nulled
                     </p>
                     <ul className="my-3 ml-6 list-disc [&>li]:mt-4">
                         <li>Deck - takes a nation name</li>
-                        <p className="ml-6 mb-2">
+                        <p className="ml-6 my-2">
                         When you pass the deck parameter in the cards route, the API will match the returned cards against the specified decks, 
                         indicating whether each card is included within them. Mutually exclusive with deck.
                         </p>
                         <li>Collection - takes an ID</li>
-                        <p className="ml-6 mb-2">
+                        <p className="ml-6 my-2">
                         When you pass the collection parameter in the cards route, the API will match the returned cards against the specified collections, 
                         indicating whether each card is included within them. Mutually exclusive with collection.
                         </p>
@@ -119,33 +154,10 @@ export default function Docs() {
                         <p className="ml-6 my-2">Format: season=1, season=2, season=3</p>
                         <li>Trophies - WA Census Rankings, 1t means #1, 1 1%, 5 5%, 10 10%</li>
                         <p className="ml-6 my-2">Format: trophies=happy-1t</p>
-                        <p className="ml-6 mb-2">Possible Trophies:</p>
-                        <ul className="ml-6 list-disc [&>li]:mt-2 [&>li]:ml-4 [&>li]:text-sm">
-                            <li>Sans</li>
-                            {Object.entries(trophiesDict).sort().map(([key, value]) => {
-                                const formattedKey = key.split(' ')
-                                    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-                                    .join(' ');
-                                return (
-                                <li key={formattedKey}>
-                                    {formattedKey} -&gt; <span className="font-bold">{value}</span>
-                                </li>
-                                );
-                            })}
-                        </ul>
                         <li>Badges</li>
                         <p className="ml-6 my-2">Format: badges=admin</p>
-                        <p className="ml-6 mb-2">Possible Badges:</p>
-                        <ul className="ml-6 list-disc [&>li]:mt-2 [&>li]:ml-4 [&>li]:text-sm">
-                            <li>Sans</li>
-                            {badges.map(badge => <li key={badge}>{badge}</li>)}
-                        </ul>
                         <li>Category - These are WA government categories</li>
                         <p className="ml-6 my-2">Format: category=anarchy</p>
-                        <p className="ml-6 mb-2">Possible Categories:</p>
-                        <ul className="ml-6 list-disc [&>li]:mt-2 [&>li]:ml-4 [&>li]:text-sm">
-                            {governments.map(government => <li key={government}>{government}</li>)}
-                        </ul>
                         <li>Cardcategory (Rarity) - You know these</li>
                         <p className="ml-6 my-2">Format: cardcategory=legendary</p>
                         <li>Region - Any region</li>
@@ -154,10 +166,6 @@ export default function Docs() {
                         <p className="ml-6 my-2">Format: exnation</p>
                         <li>Flag</li>
                         <p className="ml-6 my-2">Format: flag=afghanistan</p>
-                        <p className="ml-6 mb-2">Possible Flags:</p>
-                        <ul className="ml-6 list-disc [&>li]:mt-2 [&>li]:ml-4 [&>li]:text-sm">
-                            {flags.map(flag => <li key={flag}>{flag}</li>)}
-                        </ul>
                         <li>Motto - searches motto, currently only supports partial</li>
                         <p className="ml-6 my-2">Format: motto=lion</p>
                         <li>Name - Nation names, currently partial by default</li>
@@ -171,6 +179,7 @@ export default function Docs() {
                     <p className="leading-7 [&:not(:first-child)]:mt-6">
                         A secondary route, the purpose of this route is to take one or multiple collections, and compare it against
                         the cards in your deck. Essentially this returns all cards in your deck that are not in active collections.
+                        This only really works with the interface, as the interface provides the valid structure this route accepts.
                     </p>
                     <h4 className="scroll-m-20 text-xl font-semibold tracking-tight mt-6">
                         Exposed Parameters
