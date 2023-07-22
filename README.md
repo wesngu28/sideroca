adf2c29e66d1fe17080480cc1e3b29cb2d24846c
scramble <60171023+wesngu28@users.noreply.github.com>
2023-07-21 21:30:18 -0700

    refactor: dev layout
    * dev: dev page changes
    
    * dev: more dev changes, finalize domain move
    
    * ci: on pr
    
    * refactor: resize img, vendored ns styles, shadcn

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Card Queries
+# Sideroca
 
-Card Queries is a full-stack web application built using Next.js and FastAPI. The app serves as a user interface to interact with a PostgreSQL database containing the trading card dumps provided by NationStates.
+Sideroca (Simplifying Interactions and Dynamic Enhancements for Remarkable Outcomes in Card Acquisition) is a full-stack web application built using Next.js and FastAPI. The app serves as a user interface to interact with a PostgreSQL database containing the trading card dumps provided by NationStates.
 
-Card Queries does not aim to replicate or dream to match r3n's card queries, but it does seek to provide similar functionalities by enabling users to retrieve cards based on desired parameters.
+Sideroca does not aim to replicate or dream to match r3n's card queries, but it does seek to provide similar functionalities by enabling users to retrieve cards based on desired parameters.
+
+### Why is this called Sideroca?
+I already owned a domain sideroca.com and was not using it.
 
 <img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"> <img src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"> <img src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"> 
 
--- frontend/app/dev/page.tsx
+++ frontend/app/dev/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Home } from "lucide-react";
+import '../styles.css'
+import { S1S2Card } from "../(components)/S1S2Card";
+import { S3Card } from "../(components)/S3Card";
 
 export default function Dev() {
     return (
@@ -12,75 +15,119 @@ export default function Dev() {
             <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
                 Stack
             </h2>
-            <div className="space-y-10 sm:grid sm:grid-flow-col sm:grid-cols-2 sm:grid-rows-3 sm:gap-x-6 sm:gap-y-14 sm:space-y-0 md:grid-cols-3 md:grid-rows-2 lg:gap-x-8">
-                <div className="relative flex flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-sm transition-colors hover:border-purple-200">
-                    <div className="flex items-center bg-white/10 p-2 pl-5 transition-colors hover:bg-white/5">
-                        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" 
-                            className="text-lg font-medium leading-6 md:text-xl h-full rounded-md before:absolute before:inset-0 before:content-[''] focus:no-underline hover:no-underline active:no-underline">
-                            Next.js
-                        </a>
-                    </div>
-                    <div className="m-6 h-full text-sm md:text-base">
-                        Used by some of the world&apos;s largest companies, Next.js enables you to create full-stack Web applications by extending 
-                        the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
-                    </div>
-                </div>
-                <div className="relative flex flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-sm transition-colors hover:border-purple-200">
-                    <div className="flex items-center bg-white/10 p-2 pl-5 transition-colors hover:bg-white/5 ">
-                        <a href="https://fastapi.tiangolo.com/" target="_blank" rel="noopener noreferrer" 
-                            className="text-lg font-medium leading-6 md:text-xl h-full rounded-md before:absolute before:inset-0 before:content-[''] focus:no-underline hover:no-underline active:no-underline">
-                            FastAPI
-                        </a>
-                    </div>
-                    <div className="m-6 h-full text-sm md:text-base">
-                        FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.7+ based on standard Python type hints.
-                    </div>
-                </div>
-                <div className="relative flex flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-sm transition-colors hover:border-purple-200">
-                    <div className="flex items-center bg-white/10 p-2 pl-5 transition-colors hover:bg-white/5">
-                        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" 
-                        className="text-lg font-medium leading-6 md:text-xl h-full rounded-md before:absolute before:inset-0 before:content-[''] focus:no-underline hover:no-underline active:no-underline">
-                            Tailwind CSS
-                        </a>
-                    </div>
-                    <div className="m-6 h-full text-sm md:text-base">
-                        A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build 
-                        any design, directly in your markup.
-                    </div>
-                </div>
-                <div className="relative flex flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-sm transition-colors hover:border-purple-200">
-                    <div className="flex items-center bg-white/10 p-2 pl-5 transition-colors hover:bg-white/5">
-                        <a href="https://ui.shadcn.com/" target="_blank" rel="noopener noreferrer" 
-                        className="text-lg font-medium leading-6 md:text-xl h-full rounded-md before:absolute before:inset-0 before:content-[''] focus:no-underline hover:no-underline active:no-underline">
-                            shadcn/ui
-                        </a>
-                    </div>
-                    <div className="m-6 h-full text-sm md:text-base">
-                    Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
-                    </div>
-                </div>
-                <div className="relative flex flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-sm transition-colors hover:border-purple-200">
-                    <div className="flex items-center bg-white/10 p-2 pl-5 transition-colors hover:bg-white/5">
-                        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" 
-                        className="text-lg font-medium leading-6 md:text-xl h-full rounded-md before:absolute before:inset-0 before:content-[''] focus:no-underline hover:no-underline active:no-underline">
-                            TypeScript
-                        </a>
-                    </div>
-                    <div className="m-6 h-full text-sm md:text-base">
-                        TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.
-                    </div>
-                </div>
-                <div className="relative flex flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-sm transition-colors hover:border-purple-200">
-                    <div className="flex items-center bg-white/10 p-2 pl-5 transition-colors hover:bg-white/5">
-                        <a href="https://railway.app/" target="_blank" rel="noopener noreferrer" 
-                        className="text-lg font-medium leading-6 md:text-xl h-full rounded-md before:absolute before:inset-0 before:content-[''] focus:no-underline hover:no-underline active:no-underline">
-                            Railway
-                        </a>
-                    </div>
-                    <div className="m-6 h-full text-sm md:text-base">
-                        Made for any language, for projects big and small. Railway is the cloud that takes the complexity out of shipping software.
-                    </div>
-                </div>
+            <div className="flex flex-wrap justify-center">
+            <S1S2Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "uncommon",
+                category: "Frontend Framework",
+                flag: "./NextJS.png",
+                id: 2,
+                motto: "The React Framework for the Web",
+                name: "Next.js",
+                region: "Vercel",
+                season: 2,
+                type: "React Framework"
+            }} opt="Used by some of the world's largest companies, Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds." url="https://nextjs.org/" />
+            <S1S2Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "rare",
+                category: "CSS Framework",
+                flag: "./Tailwind.png",
+                id: 3,
+                motto: "Rapidly build modern websites without ever leaving your HTML.",
+                name: "TailwindCSS",
+                region: "Tailwind Labs",
+                season: 2,
+                type: "CSS Utility"
+            }} opt="A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup." url="https://tailwindcss.com/" />
+            <S1S2Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "legendary",
+                category: "CSS Components",
+                flag: "./Shadcn.png",
+                id: 1,
+                motto: "Build your component library.",
+                name: "shadcn/ui",
+                region: "shadcn",
+                season: 2,
+                type: "CSS Components"
+            }} opt="Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source." url="https://ui.shadcn.com/" />
+            </div>
+            <div className="flex flex-wrap justify-center">
+                <S1S2Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "common",
+                category: "SQL Database",
+                flag: "./PostgreSQL.png",
+                id: 4,
+                motto: "The World's Most Advanced Open Source Relational Database.",
+                name: "PostgreSQL",
+                region: "PostgreSQL Group",
+                season: 1,
+                type: "Database"
+            }} opt="PostgreSQL is a powerful, open source object-relational database system with over 35 years of active development that has earned it a strong reputation for reliability, feature robustness, and performance." url="https://www.postgresql.org/" />
+            <S1S2Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "rare",
+                category: "Data Cache",
+                flag: "./Redis.png",
+                id: 5,
+                motto: "A vibrant, open source database ",
+                name: "Redis",
+                region: "Redis Ltd",
+                season: 1,
+                type: "In-Memory Data Store"
+            }} opt="The open source, in-memory data store used by millions of developers as a database, cache, streaming engine, and message broker." url="https://redis.io/" />
+            <S1S2Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "ultra-rare",
+                category: "Web Framework",
+                flag: "./FastAPI.png",
+                id: 6,
+                motto: "FastAPI framework, high performance, easy to learn, fast to code, ready for production",
+                name: "FastAPI",
+                region: "Tiangolo",
+                season: 1,
+                type: "Python Framework"
+            }} opt="FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.7+ based on standard Python type hints." url="https://fastapi.tiangolo.com/" />
+            </div>
+            <div className="flex flex-wrap justify-center">
+            <S3Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "rare",
+                category: "Containers",
+                flag: "./Docker.png",
+                id: 7,
+                motto: "Develop faster. Run anywhere.",
+                name: "Docker",
+                region: "Docker Inc",
+                season: 3,
+                type: "Virtualization"
+            }} opt="Accelerate how you build, share, and run modern applications. Docker makes development efficient and predictable." url="https://www.docker.com/" />
+            <S3Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "epic",
+                category: "Continuous Integration",
+                flag: "./GithubActions.png",
+                id: 8,
+                motto: "Automate your workflow from idea to production",
+                name: "GitHub Actions",
+                region: "GitHub",
+                season: 3,
+                type: "Development Workflow"
+            }} opt="GitHub Actions makes it easy to automate all your software workflows, now with world-class CI/CD." url="https://docs.github.com/en/actions" />
+            <S3Card card={{
+                badges: {}, description: "", trophies: {},
+                cardcategory: "legendary",
+                category: "Cloud Provider",
+                flag: "./Hetzner.png",
+                id: 9,
+                motto: "Experience the Hetzner Cloud difference!.",
+                name: "Hetzner",
+                region: "Hetzner",
+                season: 3,
+                type: "Virtual Private Server"
+            }} opt="A little money gets you lots of cloud. Flexible cloud servers with high-end-hardware." url="https://www.hetzner.com/" />
             </div>
             <p className="leading-7 [&:not(:first-child)]:mt-6">Credit to UPC/r3n for API inspiration.</p>
         </main>
