e6f7a67d085dc167e310928e1948bb4e80acbd46
wesngu28 <wesngu028@gmail.com>
2023-08-08 10:08:58 -0700

    refactor: seo and gitignore

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from './(components)/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Cards',
-  description: 'Get cards',
+  title: 'Sideroca - Card Queries',
+  description: 'Streamlined Interactions and Dynamic Enhancements for Really Obtuse Cards Applications',
 }
 
 export default function RootLayout({
@@ -17,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head><Script async src="https://nakiri.vercel.app/script.js" data-website-id="9ec15190-e6d0-43fe-a366-b99ffba27425" /></head>
+      <head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+        <Script async src="https://nakiri.vercel.app/script.js" data-website-id="9ec15190-e6d0-43fe-a366-b99ffba27425" />
+      </head>
       <body className={`${inter.className} flex items-center flex-col`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
